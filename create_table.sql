b34e6ae772c7514531b45ff2ebd95a6d294c4448
lastdrv <lastdrv@gmail.com>
2021-05-01 18:42:43 +0300

    add readme, sql and template data

---
+++ create_table.sql
@@ -0,0 +1,18 @@
+create table test.facebook
+(
+    phone       int unsigned not null primary key,
+    id          bigint       not null,
+    first_name  varchar(100) null,
+    second_name varchar(100) null,
+    sex         varchar(100) null,
+    city        varchar(500) null,
+    `restrict`  varchar(500) null,
+    status      varchar(100) null,
+    work        varchar(500) null,
+    date        varchar(100) null,
+    unknown     varchar(500) null,
+    birthday    varchar(100) null
+);
+
+create index facebook_phone_IDX
+    on test.facebook (phone);
